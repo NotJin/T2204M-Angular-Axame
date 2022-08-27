d8e82a86ea733afc3d3a9def8b1deccc4ea5c026
Đồng Vũ <Dongvu7173@gmail.com>
2022-08-27 13:21:16 +0700

    exam

--- src/app/axame/exame.component.ts
+++ src/app/axame/exame.component.ts
@@ -6,15 +6,8 @@ import {Component} from "@angular/core";
 })
 
 export class ExameComponent{
-  show:boolean = false
-  show1:boolean = false
   ngOnInit(){
 
   }
-  show_Type(){
-    this.show =! this.show
-  }
-  show_Type2(){
-    this.show1 =! this.show1
-  }
+
 }
